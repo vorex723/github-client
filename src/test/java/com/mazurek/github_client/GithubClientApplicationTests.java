323a2d0e9271b203d7b95dda1754efa0f16f0c29
Mateusz Mazurek/Win <mateuszmazurek@protonmail.com>
2024-08-07 12:37:11 +0200

    init commit

---
+++ src/test/java/com/mazurek/github_client/GithubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazurek.github_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
